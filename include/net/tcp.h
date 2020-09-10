a58f3d11a14725f6716e7103173797da977f4f24
Neal Cardwell <ncardwell@google.com>
2020-09-10 15:35:34 -0400

    UPSTREAM: tcp: simplify tcp_set_congestion_control(): Always reinitialize
    Now that the previous patches ensure that all call sites for
    tcp_set_congestion_control() want to initialize congestion control, we
    can simplify tcp_set_congestion_control() by removing the reinit
    argument and the code to support it.
    
    Change-Id: I540be122f644b05c35887358aee1484b8d04f65f
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yuchung Cheng <ycheng@google.com>
    Acked-by: Kevin Yang <yyd@google.com>
    Cc: Lawrence Brakmo <brakmo@fb.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1281,11 +1281,12 @@ void tcp_get_available_congestion_control(char *buf, size_t len);
 void tcp_get_allowed_congestion_control(char *buf, size_t len);
 int tcp_set_allowed_congestion_control(char *allowed);
 int tcp_set_congestion_control(struct sock *sk, const char *name, bool load,
-			       bool reinit, bool cap_net_admin);
+			    bool cap_net_admin);
 #ifdef CONFIG_MPTCP
 int __tcp_set_congestion_control(struct sock *sk, const char *name, bool load,
 				 bool reinit, bool cap_net_admin);
 #endif
+
 u32 tcp_slow_start(struct tcp_sock *tp, u32 acked);
 void tcp_cong_avoid_ai(struct tcp_sock *tp, u32 w, u32 acked);
 
