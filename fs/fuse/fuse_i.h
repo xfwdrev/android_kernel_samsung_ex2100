aaebd2139602e07701a7bd986d54e507260ff33c
Amir Goldstein <amir73il@gmail.com>
2021-06-21 14:03:53 +0300

    UPSTREAM: fuse: fix illegal access to inode with reused nodeid
    commit 15db16837a35d8007cb8563358787412213db25e upstream.
    
    Server responds to LOOKUP and other ops (READDIRPLUS/CREATE/MKNOD/...)
    with ourarg containing nodeid and generation.
    
    If a fuse inode is found in inode cache with the same nodeid but different
    generation, the existing fuse inode should be unhashed and marked "bad" and
    a new inode with the new generation should be hashed instead.
    
    This can happen, for example, with passhrough fuse filesystem that returns
    the real filesystem ino/generation on lookup and where real inode numbers
    can get recycled due to real files being unlinked not via the fuse
    passthrough filesystem.
    
    With current code, this situation will not be detected and an old fuse
    dentry that used to point to an older generation real inode, can be used to
    access a completely new inode, which should be accessed only via the new
    dentry.
    
    Note that because the FORGET message carries the nodeid w/o generation, the
    server should wait to get FORGET counts for the nlookup counts of the old
    and reused inodes combined, before it can free the resources associated to
    that nodeid.
    
    Stable backport notes:
    * This is not a regression. The bug has been in fuse forever, but only
      a certain class of low level fuse filesystems can trigger this bug
    * Because there is no way to check if this fix is applied in runtime,
      libfuse test_examples.py tests this fix with hardcoded check for
      kernel version >= 5.14
    * After backport to stable kernel(s), the libfuse test can be updated
      to also check minimal stable kernel version(s)
    * Depends on "fuse: fix bad inode" which is already applied to stable
      kernels v5.4.y and v5.10.y
    * Required backporting helper inode_wrong_type()
    
    Change-Id: I5f92158bc8ccdb50627fb035bb28d3ade51820b3
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/linux-fsdevel/CAOQ4uxi8DymG=JO_sAU+wS8akFdzh+PuXwW3Ebgahd2Nwnh7zA@mail.gmail.com/
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -907,6 +907,13 @@ static inline u64 fuse_get_attr_version(struct fuse_conn *fc)
 	return atomic64_read(&fc->attr_version);
 }
 
+static inline bool fuse_stale_inode(const struct inode *inode, int generation,
+				    struct fuse_attr *attr)
+{
+	return inode->i_generation != generation ||
+		inode_wrong_type(inode, attr->mode);
+}
+
 static inline void fuse_make_bad(struct inode *inode)
 {
 	set_bit(FUSE_I_BAD, &get_fuse_inode(inode)->state);
