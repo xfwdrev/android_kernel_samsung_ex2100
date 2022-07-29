7060cf713124065ff5bec8b238cae5639d647229
Todd Kjos <tkjos@google.com>
2022-07-29 00:59:18 +0000

    UPSTREAM: ANDROID: fix kernelci error in fs/fuse/dir.c
    kernelci reported the following error when !CONFIG_FUSE_BPF:
    
    fs/fuse/dir.c:269:40: error: no member named 'backing_inode' in 'struct fuse_inode'
    
    Fixed by wrapping that new code in #ifdef CONFIG_FUSE_BPF.
    
    Fixes: 094905c87771 ("ANDROID: fuse-bpf: Always call revalidate for
    backing")
    Signed-off-by: Todd Kjos <tkjos@google.com>
    Change-Id: I08760f340cebf21d3084ca84ac1792bc70f311ff

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -261,6 +261,7 @@ static int fuse_dentry_revalidate(struct dentry *entry, unsigned int flags)
 
 		parent = dget_parent(entry);
 
+#ifdef CONFIG_FUSE_BPF
 		/* TODO: Once we're handling timeouts for backing inodes, do a
 		 * bpf based lookup_revalidate here.
 		 */
@@ -269,7 +270,7 @@ static int fuse_dentry_revalidate(struct dentry *entry, unsigned int flags)
 			ret = 1;
 			goto out;
 		}
-
+#endif
 		forget = fuse_alloc_forget();
 		ret = -ENOMEM;
 		if (!forget) {
