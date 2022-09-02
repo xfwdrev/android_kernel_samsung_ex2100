34dd8c019a5c225fa9e0cf36d61bd275e2950dcf
Martin KaFai Lau <martin.lau@kernel.org>
2022-09-01 17:27:56 -0700

    UPSTREAM: net: Change sock_getsockopt() to take the sk ptr instead of the sock ptr
    [ Upstream commit ba74a7608dc12fbbd8ea36e660087f08a81ef26a ]
    
    A latter patch refactors bpf_getsockopt(SOL_SOCKET) with the
    sock_getsockopt() to avoid code duplication and code
    drift between the two duplicates.
    
    The current sock_getsockopt() takes sock ptr as the argument.
    The very first thing of this function is to get back the sk ptr
    by 'sk = sock->sk'.
    
    bpf_getsockopt() could be called when the sk does not have
    the sock ptr created.  Meaning sk->sk_socket is NULL.  For example,
    when a passive tcp connection has just been established but has yet
    been accept()-ed.  Thus, it cannot use the sock_getsockopt(sk->sk_socket)
    or else it will pass a NULL ptr.
    
    This patch moves all sock_getsockopt implementation to the newly
    added sk_getsockopt().  The new sk_getsockopt() takes a sk ptr
    and immediately gets the sock ptr by 'sock = sk->sk_socket'
    
    The existing sock_getsockopt(sock) is changed to call
    sk_getsockopt(sock->sk).  All existing callers have both sock->sk
    and sk->sk_socket pointer.
    
    The latter patch will make bpf_getsockopt(SOL_SOCKET) call
    sk_getsockopt(sk) directly.  The bpf_getsockopt(SOL_SOCKET) does
    not use the optnames that require sk->sk_socket, so it will
    be safe.
    
    Change-Id: Iea2d7c10885f8eac7bbf715fd725fa810baa1eef
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Link: https://lore.kernel.org/r/20220902002756.2887884-1-kafai@fb.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Stable-dep-of: 5a287d3d2b9d ("lsm: fix default return value of the socket_getpeersec_*() hooks")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/core/sock.c
+++ net/core/sock.c
@@ -1351,10 +1351,10 @@ static int groups_to_user(gid_t __user *dst, const struct group_info *src)
 	return 0;
 }
 
-int sock_getsockopt(struct socket *sock, int level, int optname,
-		    char __user *optval, int __user *optlen)
+static int sk_getsockopt(struct sock *sk, int level, int optname,
+			 char __user *optval, int __user *optlen)
 {
-	struct sock *sk = sock->sk;
+	struct socket *sock = sk->sk_socket;
 
 	union {
 		int val;
@@ -1690,6 +1690,12 @@ lenout:
 	if (put_user(len, optlen))
 		return -EFAULT;
 	return 0;
+}
+
+int sock_getsockopt(struct socket *sock, int level, int optname,
+		    char __user *optval, int __user *optlen)
+{
+	return sk_getsockopt(sock->sk, level, optname, optval, optlen);
 }
 
 /*
