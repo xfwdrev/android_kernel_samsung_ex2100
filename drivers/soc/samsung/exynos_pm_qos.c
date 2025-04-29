607a18841cd1e7918fc135731317ae0fb5b1b499
xfwdrev <frignacio16@gmail.com>
2025-04-29 23:56:02 +0800

    Fix some typo
    We all make mistakes sometimes

--- drivers/soc/samsung/exynos_pm_qos.c
+++ drivers/soc/samsung/exynos_pm_qos.c
@@ -1064,7 +1064,7 @@ void exynos_pm_qos_update_request_timeout(struct exynos_pm_qos_request *req, s32
 			exynos_pm_qos_array[req->exynos_pm_qos_class]->constraints,
 			&req->node, EXYNOS_PM_QOS_UPDATE_REQ, new_value);
 
-		queue_delayed_work(system_power_efficient_wq, &req->work, usecs_to_jiffies(timeout_us));
+	queue_delayed_work(system_power_efficient_wq, &req->work, usecs_to_jiffies(timeout_us));
 }
 EXPORT_SYMBOL_GPL(exynos_pm_qos_update_request_timeout);
 
