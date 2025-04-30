3f2741faf1f85583c85d050625ed9c9b2658d7b8
afaneh92 <mohammad.afaneh92@gmail.com>
2025-04-30 13:52:47 +0800

    selinux: Add toggable selinux boot status

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -160,6 +160,15 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 
 	old_value = enforcing_enabled(state);
 
+
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
+	// If always enforce option is set, selinux is always enforcing
+	new_value = 1;
+#elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
+	// If always permissive option is set, selinux is always permissive
+	new_value = 0;
+#endif
+
 // [ SEC_SELINUX_PORTING_COMMON
 #ifdef CONFIG_ALWAYS_ENFORCE
 	// If build is user build and enforce option is set, selinux is always enforcing
