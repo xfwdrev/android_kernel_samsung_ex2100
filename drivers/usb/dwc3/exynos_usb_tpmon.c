8deeb1ceb99766b878891d4b5b95a6a5bed33c51
xfwdrev <frignacio16@gmail.com>
2025-06-03 23:35:17 +0800

    treewide: fix clang build errors

--- drivers/usb/dwc3/exynos_usb_tpmon.c
+++ drivers/usb/dwc3/exynos_usb_tpmon.c
@@ -79,7 +79,7 @@ int usb_tpmon_check_tp(ktime_t current_time, int data_size)
 	return 0;
 }
 
-void usb_tpmon_init_data()
+void usb_tpmon_init_data(void)
 {
 	pr_info("Intialization USB TPMON data.\n");
 	tpmon.accumulated_time = 0;
@@ -115,7 +115,7 @@ static void usb_tpmon_work(struct work_struct *data)
 	dwc3_otg_pm_ctrl(g_dwc, 0);
 }
 
-void usb_tpmon_init()
+void usb_tpmon_init(void)
 {
 	pr_info("%s\n", __func__);
 
@@ -126,20 +126,20 @@ void usb_tpmon_init()
 	
 }
 
-void usb_tpmon_exit()
+void usb_tpmon_exit(void)
 {
 	pr_info("%s\n", __func__);
 	destroy_workqueue(tpmon.tpmon_wq);
 }
 
-void usb_tpmon_open()
+void usb_tpmon_open(void)
 {
 	pr_info("%s\n", __func__);
 	usb_tpmon_init_data();
 }
 
 /* Call this function at removing USB */
-void usb_tpmon_close()
+void usb_tpmon_close(void)
 {
 	pr_info("%s\n", __func__);
 	pr_info("USB TPMON - Unlock all.\n");
