a7ee58cd6e7e0f4cebc21fe3a7bef9f13e754bc5
Flopster101 <nahuelgomez329@gmail.com>
2026-03-19 21:14:49 -0300

    block: iosched: Add option to set ADIOS as default scheduler
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- block/elevator.c
+++ block/elevator.c
@@ -688,6 +688,8 @@ void elevator_init_mq(struct request_queue *q)
 		e = elevator_get(q, "kyber", false);
 	} else if (IS_ENABLED(CONFIG_MQ_SSG_DEFAULT)) {
 		e = elevator_get(q, "ssg", false);
+	} else if (IS_ENABLED(CONFIG_MQ_ADIOS_DEFAULT)) {
+		e = elevator_get(q, "adios", false);
 	} else if (!q->required_elevator_features)
 		e = elevator_get_default(q);
 	else
