5981989353649cc9136f651045659d18350e0ac9
xfwdrev <74693315+xfwdrev@users.noreply.github.com>
2026-05-01 21:24:40 +0800

    drivers: sec_battery: flip chg_mode
    Well, SEC_BAT_CHG_MODE_BUCK_OFF from the word itself disables the buck, cuts off the charger completely, and still drains the battery. Basically it acted like the charger isn't connected at all.
    
    We want this function to actually pull power from the charger not the battery so we use SEC_BAT_CHG_MODE_CHARGING_OFF that only stops the battery from charging while maintaining its current charge capacity.
    
    Signed-off-by: xfwdrev <74693315+xfwdrev@users.noreply.github.com>

--- drivers/battery/common/sec_battery.c
+++ drivers/battery/common/sec_battery.c
@@ -3465,7 +3465,7 @@ static void sec_bat_check_store_mode(struct sec_battery_info *battery)
 		return;
 
 	if (!is_nocharge_type(battery->cable_type) && !battery->charging_enabled) {
-		int chg_mode = SEC_BAT_CHG_MODE_CHARGING_OFF;
+		int chg_mode = SEC_BAT_CHG_MODE_BUCK_OFF;
 
 		pr_info("%s: capacity(%d), status(%d), charging_enabled(%d)\n",
 			 __func__, battery->capacity, battery->status, battery->charging_enabled);
@@ -3473,7 +3473,7 @@ static void sec_bat_check_store_mode(struct sec_battery_info *battery)
 		/* Keep bypass on when requested, with low-SOC safety fallback. */
 		if (battery->capacity > battery->pdata->store_mode_charging_max ||
 				battery->pdata->store_mode_buckoff)
-			chg_mode = SEC_BAT_CHG_MODE_BUCK_OFF;
+			chg_mode = SEC_BAT_CHG_MODE_CHARGING_OFF;
 
 		sec_bat_set_charging_status(battery, POWER_SUPPLY_STATUS_DISCHARGING);
 		sec_bat_set_charge(battery, chg_mode);
