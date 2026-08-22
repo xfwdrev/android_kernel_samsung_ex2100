d6943cd9fca7f6eb1b36b580f57acd13755401c0
Flopster101 <nahuelgomez329@gmail.com>
2026-08-21 21:29:51 -0300

    media: mfc: Disable lazy unmap on ION builds
    Lazy unmap keeps IOVA mappings alive until the backing buffer is
    released, fragmenting the MFC domain until large allocations fail.
    
    [55786.822534] __alloc_and_insert_iova_range: size 1793 limit 851969 dma32bitpfn 1048576 max32 1793 align 1
    [55790.067862] Error getting dmabuf scatterlist
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/media/platform/exynos/mfc/mfc_mem.c
+++ drivers/media/platform/exynos/mfc/mfc_mem.c
@@ -871,6 +871,11 @@ void mfc_check_iova(struct mfc_dev *dev)
 	struct mfc_ctx *ctx;
 	unsigned long total_iova = 0;
 
+	if (!IS_ENABLED(CONFIG_EXPERIMENTAL_DMA_BUF)) {
+		dev->skip_lazy_unmap = 1;
+		return;
+	}
+
 	if (!pdata->iova_threshold)
 		return;
 
